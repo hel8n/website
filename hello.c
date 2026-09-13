0c69477bedfc01bc08dec13801dc7f293e9c14ef
Helen Grishina <el.gr1846@yandex.ru>
2026-09-13 16:13:37 +0300

    новые файлы

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    const char *letter  = "П";
+    const char *letter1 = "Р";
+    const char *letter2 = "И";
+    const char *letter3 = "В";
+    const char *letter4 = "Е";
+    const char *letter5 = "Т";
+
+    printf("%s\n", letter);
+    printf("%s\n", letter1);
+    printf("%s\n", letter2);
+    printf("%s\n", letter3);
+    printf("%s\n", letter4);
+    printf("%s\n", letter5);
+    return 0;
+}
